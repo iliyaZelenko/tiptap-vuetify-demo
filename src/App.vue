dff3d32b6b9268b9bf4c2d936d7aa84085a1c340
Ilya <iliyazelenkog@gmail.com>
2020-02-01 10:03:48 +0200

    feat(UI): new style for site

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,92 @@
 <template>
   <div id="app">
     <v-app>
-      <v-content>
-        <v-container>
-          <h1 style="overflow: auto;">Tiptap Vuetify
-            <v-btn href="https://github.com/iliyaZelenko/tiptap-vuetify" style="float: right;" icon>
-              <v-icon>mdi-github-circle</v-icon>
-            </v-btn>
-          </h1>
-          <v-divider/>
+      <v-app-bar
+        color="#fcb69f"
+        src="https://i.picsum.photos/id/282/1920/200.jpg"
+        absolute
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+          ></v-img>
+        </template>
 
+        <v-toolbar-title class="white--text">Tiptap Vuetify</v-toolbar-title>
+
+        <v-spacer />
+
+        <div>
+          <v-btn
+            href="https://github.com/iliyaZelenko/tiptap-vuetify"
+            color="orange"
+          >
+            <v-icon left>mdi-github-circle</v-icon>
+            Github
+          </v-btn>
+          <v-btn
+            class="mx-2"
+            href="https://github.com/iliyaZelenko/tiptap-vuetify-demo"
+            color="purple"
+            dark
+          >
+            <v-icon left>mdi-github-circle</v-icon>
+            Demo Source Code
+          </v-btn>
+          <v-btn
+            href="https://www.patreon.com/zelen"
+            color="black"
+            dark
+          >
+            <v-icon left>mdi-currency-usd</v-icon>
+            Donate
+          </v-btn>
+        </div>
+
+        <template v-slot:extension>
+          <v-tabs
+            align-with-title
+            dark
+          >
+            <v-tab
+              v-for="route of routes"
+              :key="route.name"
+              :to="route.path"
+              color="primary"
+              text
+              exact
+            >
+              {{ route.textInfo }}
+            </v-tab>
+
+            <v-spacer />
+
+            <div class="mr-4">
+              <v-btn
+                class="mx-2"
+                href="https://github.com/iliyaZelenko/tiptap-vuetify/issues"
+                color="green"
+              >
+                <v-icon left>mdi-lifebuoy</v-icon>
+                Get free help
+              </v-btn>
+              <v-btn
+                href="https://github.com/iliyaZelenko/tiptap-vuetify/issues/133"
+                color="red"
+                dark
+              >
+                <v-icon left>mdi-handshake</v-icon>
+                Get paid help
+              </v-btn>
+            </div>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+
+      <v-content style="margin-top: 150px;">
+        <v-container>
+          <!--
           <h2 class="text-center my-3">Examples navigation</h2>
 
           <div class="text-center mb-4">
@@ -20,10 +97,13 @@
               color="primary"
               text
               exact
-            >{{ route.textInfo }}</v-btn>
+            >
+              {{ route.textInfo }}
+            </v-btn>
           </div>
+          -->
 
-          <switch-icons-group/>
+          <switch-icons-group />
         </v-container>
 
         <div>
@@ -34,6 +114,19 @@
               </v-container>
             </v-card-text>
           </v-card>
+        </div>
+
+        <div class="text-center mb-4">
+          To support the project, put a star
+          <v-icon color="black">mdi-star</v-icon>
+          on the
+          <a href="https://github.com/iliyaZelenko/tiptap-vuetify">
+            github
+          </a>
+          🙏 (this is easy), thanks!
+          <a href="https://share.getcloudapp.com/8LuwOKdR">
+            Where to click?
+          </a>
         </div>
       </v-content>
     </v-app>
